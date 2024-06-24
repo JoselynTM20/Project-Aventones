8b1402d5d4d11ff85d940afd6a24209b2b120dbe
Joselyn Tijerino Molina <113789740+JoselynTM20@users.noreply.github.com>
2024-06-23 22:58:58 -0600

    -Encriptacion de password -Arreglo de datos en bd -Agregar colección de drivers -Registro de drivers en bd

--- Project1Web2/index.js
+++ Project1Web2/index.js
@@ -13,14 +13,21 @@ app.use(cors({
 }));
 
 //para la conexion a bd
-const db = mongoose.connect("mongodb+srv://lingama04:1234@cluster0.qlrltgq.mongodb.net/users");
-//const db = mongoose.connect("mongodb+srv://JoselynTijerino:JoselynTijerino15@cluster0.bzbj5gg.mongodb.net/users");
+//const db = mongoose.connect("mongodb+srv://lingama04:1234@cluster0.qlrltgq.mongodb.net/users");
+const db = mongoose.connect("mongodb+srv://JoselynTijerino:JoselynTijerino15@cluster0.bzbj5gg.mongodb.net/users");
 
 const { UserPost, UserGet, UserPut, UserDelete } = require('../Project1Web2/BackEend/controllers/usersControllers');
 app.get("/api/user/", UserGet);
 app.post("/api/user/",  UserPost);
-app.put("/api/user", UserPut);
-app.delete("/api/user", UserDelete);
+////app.put("/api/user", UserPut);
+//app.delete("/api/user", UserDelete);
+
+
+const { DriverPost, DriverGet } = require('../Project1Web2/BackEend/controllers/driversControllers');
+// Rutas para conductores
+app.get("/api/driver/", DriverGet);
+app.post("/api/driver/", DriverPost);
+
 
 // Iniciar el servidor
 const port = 3000;
