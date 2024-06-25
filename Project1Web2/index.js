4d99129801e8aecdbed9cc98147b38e692d280d8
Joselyn Tijerino Molina <113789740+JoselynTM20@users.noreply.github.com>
2024-06-24 21:28:22 -0600

    commit arreglos crud

--- Project1Web2/index.js
+++ Project1Web2/index.js
@@ -7,8 +7,7 @@ const path = require('path');
 
 
 const { login } = require('../Project1Web2/BackEend/controllers/authControllers');
-// Importar el modelo de Ride
-const Ride = require('../Project1Web2/BackEend/models/RidesDriversModel');
+
 
 app.use(bodyParser.json());
 app.use(cors({
@@ -25,11 +24,9 @@ const db = mongoose.connect("mongodb+srv://JoselynTijerino:JoselynTijerino15@clu
 
 app.post("/api/login", login);
 
-const { UserPost, UserGet, UserPut, UserDelete } = require('../Project1Web2/BackEend/controllers/usersControllers');
+const { UserPost, UserGet} = require('../Project1Web2/BackEend/controllers/usersControllers');
 app.get("/api/user/", UserGet);
 app.post("/api/user/",  UserPost);
-////app.put("/api/user", UserPut);
-//app.delete("/api/user", UserDelete);
 
 
 const { DriverPost, DriverGet } = require('../Project1Web2/BackEend/controllers/driversControllers');
@@ -38,28 +35,14 @@ app.get("/api/driver/", DriverGet);
 app.post("/api/driver/", DriverPost);
 
 
-const { RidesDriverPost,  RidesDriverGet, updateRideDriver} = require('../Project1Web2/BackEend/controllers/ridesControllers');
+const { RidesDriverPost,  RidesDriverGet} = require('../Project1Web2/BackEend/controllers/ridesControllers');
 app.post("/api/rides", RidesDriverPost);
 app.get("/api/rides", RidesDriverGet);
-app.put("/api/rides", updateRideDriver);
-// Ruta para eliminar un ride por su ID
-/*app.delete('/api/rides/:rideid', async (req, res) => {
-    const rideId = req.params.rideid;
 
-    try {
-        const deletedRide = await Ride.findByIdAndDelete(rideId);
-        if (!deletedRide) {
-            return res.status(404).json({ error: 'Ride not found' });
-        }
-        res.json({ message: 'Ride deleted successfully' });
-    } catch (error) {
-        console.error('Error deleting ride:', error);
-        res.status(500).json({ error: 'Failed to delete ride' });
-    }
-});*/
+
 
 
 
 // Iniciar el servidor
-const port = 3000;
+const port = 3001;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
