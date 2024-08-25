7fc2b3da06e8de05e23546c8b6534247af62bb0e
Lingama4 <Lingama04@gmail.com>
2024-08-25 08:24:16 -0600

    Updated app.js add apollo server
    25082024 0824

--- Project1Web2/index.js
+++ Project1Web2/index.js
@@ -10,6 +10,11 @@ import client from './GraphQL/Config/ApolloClient';  // Importa el archivo de co
 const { authenticateToken } = require('./BackEend/middleware/authMiddleware');
 const { login } = require('./BackEend/controllers/authControllers');
 
+// Importa los typeDefs y resolvers de GraphQL
+const typeDefs = require('./GraphQL/Schemas/index');
+const resolvers = require('./GraphQL/Resolvers/index');
+
+
 const { UserGet, UserPost } = require('./BackEend/controllers/usersControllers');
 const { DriverPost, DriverGet } = require('./BackEend/controllers/driversControllers');
 const { RidesDriverPost, RidesDriverGet, updateRideDriver, deleteRide, getRidesByDriver, RidesDriverGetById } = require('./BackEend/controllers/ridesControllers');
@@ -23,16 +28,35 @@ app.use(cors({
 
 // Para la conexión a la base de datos
 
-/*mongoose.connect("mongodb+srv://lingama04:1234@cluster0.qlrltgq.mongodb.net/users", {
+mongoose.connect("mongodb+srv://lingama04:1234@cluster0.qlrltgq.mongodb.net/users", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})*/
-
-mongoose.connect("mongodb+srv://JoselynTijerino:JoselynTijerino@cluster0.6sdzi3m.mongodb.net/users", {
 })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
+/*mongoose.connect("mongodb+srv://JoselynTijerino:JoselynTijerino@cluster0.6sdzi3m.mongodb.net/users", {
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log('MongoDB connection error:', err));*/
+
+
+
+// Configurar Apollo Server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        // Aquí puedes agregar lógica para pasar información de autenticación o contexto a los resolvers
+        const token = req.headers.authorization || '';
+        // Validar el token si es necesario y pasar datos del usuario al contexto
+        return { token };
+    }
+});
+
+// Aplicar middleware de Apollo Server con Express
+await server.start();
+server.applyMiddleware({ app });    
 // Rutas Públicas (no requieren autenticación)
 app.post('/api/login', login);
 app.post('/api/register', UserPost); // Ruta para registrar nuevos usuarios
@@ -59,4 +83,7 @@ app.delete('/api/bookings/:id', authenticateToken, DeleteBooking);
 
 // Iniciar el servidor
 const port = 3001;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Servidor REST escuchando en http://localhost:${port}`);
+    console.log(`Servidor GraphQL escuchando en http://localhost:${port}${server.graphqlPath}`);
+});
